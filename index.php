cc46e9e9f95d2c4ffe7697d2eeeb432759447737
A Sharma <as3426@njit.edu>
2017-07-16 12:19:22 -0400

    index.php created and added

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<title> Introduction </title>
+<h1> Web Systems Developement </h1>
+<body>
+<div style="height: 550px;">
+PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.
+<br/>
+MySQL is the most popular database system used with PHP.
+<br/>
+HTML is the standard markup language for creating Web pages.
+</div>
+<footer><b>
+<?php 
+ echo"date:".date("m-d-Y");
+?>
+</b>
+</footer>
+</body>
+</html>
